4cee6887be4ebb2866bf005c90cf06de7c34bc6d
Nebula Venus <6595784+Nebulavenus@users.noreply.github.com>
2020-01-12 23:44:36 +0600

    Tags parsing?

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -2,12 +2,17 @@
 extern crate proc_macro;
 extern crate syn;
 
-use quote::{format_ident, quote};
+mod attr;
+
+use quote::{quote, ToTokens};
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
+use syn::Token;
+use syn::token::Token;
+use syn::{parenthesized};
 
-#[proc_macro_derive(NMread)]
-pub fn derive_mnread(input: TokenStream) -> TokenStream {
+#[proc_macro_derive(NMread, attributes(nebula))]
+pub fn derive_nmread(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let gen = impl_try_from_ctx(&ast);
     gen.into()
@@ -30,17 +35,144 @@ fn impl_try_from_ctx(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     }
 }
 
+fn nebula_of(attrs: &Vec<syn::Attribute>) -> Option<&syn::Attribute> {
+    for attr in attrs {
+        if attr.path.segments.len() == 1 && attr.path.segments[0].ident == "nebula" {
+            return Some(attr);
+        }
+    }
+    None
+}
+
+enum Attribute {
+    Tag(syn::Expr)
+}
+
+impl syn::parse::Parse for Attribute {
+    fn parse(input: syn::parse::ParseStream) -> syn::parse::Result<Self> {
+        let key: syn::Ident = input.parse()?;
+        let value = match key.to_string().as_str() {
+            "tag" => {
+                let _: Token![=] = input.parse()?;
+                let expr: syn::Expr = input.parse()?;
+                Attribute::Tag(expr)
+            }
+            key => panic!("Unrecognized attribute: '{}'", key)
+        };
+
+        Ok(value)
+    }
+}
+
+struct Attributes(syn::punctuated::Punctuated<Attribute, Token![,]>);
+
+impl syn::parse::Parse for Attributes {
+    fn parse(input: syn::parse::ParseStream) -> syn::parse::Result<Self> {
+        let content;
+        parenthesized!(content in input);
+        Ok(Attributes(content.parse_terminated(Attribute::parse)?))
+    }
+}
+
+fn parse_tag(attr: &syn::Attribute) -> Option<proc_macro2::TokenStream> {
+    fn mk_err<T: quote::ToTokens>(t: T) -> Option<proc_macro2::TokenStream> {
+        Some(
+            syn::Error::new_spanned(t, "expected `nebula(tag = \"...\")`").to_compile_error(),
+        )
+    }
+
+    let meta = match attr.parse_meta() {
+        Ok(syn::Meta::List(mut nvs)) => {
+            // List is nebula(..)
+            if nvs.nested.len() != 1 {
+                return mk_err(nvs);
+            }
+
+            match nvs.nested.pop().unwrap().into_value() {
+                syn::NestedMeta::Meta(syn::Meta::NameValue(nv)) => {
+                    if nv.path.get_ident().unwrap() != "tag" {
+                        // Should be tag
+                        return mk_err(nvs);
+                    }
+                    nv
+                }
+                meta => {
+                    // was not k = v
+                    return mk_err(meta);
+                }
+            }
+        }
+        Ok(meta) => {
+            // was nebula = smt
+            return mk_err(meta);
+        }
+        Err(e) => {
+            return Some(e.to_compile_error());
+        }
+    };
+
+    match meta.lit {
+        syn::Lit::Int(i) => {
+            let expect_value = i.base10_parse::<u32>().expect("Integer parsing error");
+
+            let expr = quote! {
+                let read_value = src.gread_with::<u32>(offset, ctx)?;
+                if read_value != #expect_value {
+                    let hex_read = format!("{:X}", read_value);
+                    let hex_expect = format!("{:X}", expect_value);
+                    panic!("MDXFormat is not correct, expected {}, found {}", hex_expect, hex_read);
+                }
+            };
+            Some(expr)
+        },
+        lit => {
+            panic!("expected int, found {:?}", lit)
+        },
+    }
+}
+
 fn impl_struct(name: &syn::Ident, fields: &syn::FieldsNamed) -> proc_macro2::TokenStream {
     let items: Vec<_> = fields.named.iter().map(|f| {
         let ident = &f.ident;
         let ty = &f.ty;
+        let attrs = &f.attrs;
+        let tag_exp1r = {
+            if let Some(g) = nebula_of(attrs) {
+                parse_tag(g).unwrap()
+            } else {
+                quote! { }
+            }
+        };
+
+        let mut some_tag = None;
+        {
+            for attr in attrs {
+                let path = &attr.path.segments[0].ident;
+                if path == "nebula" {
+                    let parsed_attrs: Attributes = syn::parse2(attr.tokens.clone()).unwrap();
+                    for attr in parsed_attrs.0 {
+                        match attr {
+                            Attribute::Tag(expr) => some_tag = Some(expr),
+                        }
+                    }
+                }
+            }
+        };
+        dbg!(some_tag);
+
         match *ty {
             syn::Type::Array(ref array) => {
                 match array.len {
                     syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Int(ref int), ..}) => {
                         let size = int.base10_parse::<usize>().unwrap();
                         quote! {
-                            #ident: { let mut __tmp: #ty = [0; #size]; src.gread_inout_with(offset, &mut __tmp, ctx)?; __tmp }
+                            #ident: {
+                                //#tag_expr
+
+                                let mut __tmp: #ty = [0; #size];
+                                src.gread_inout_with(offset, &mut __tmp, ctx)?;
+                                __tmp
+                            }
                         }
                     },
                     _ => panic!("NMread derive with bad array constexpr")
@@ -48,7 +180,11 @@ fn impl_struct(name: &syn::Ident, fields: &syn::FieldsNamed) -> proc_macro2::Tok
             },
             _ => {
                 quote! {
-                    #ident: src.gread_with::<#ty>(offset, ctx)?
+                    #ident: {
+                        //#tag_expr
+
+                        src.gread_with::<#ty>(offset, ctx)?
+                    }
                 }
             }
         }
--- derive/tests/tests.rs
+++ derive/tests/tests.rs
@@ -1,13 +1,21 @@
 use nebula_mdx_derive_internal::NMread;
+#[allow(unused_imports)]
+use scroll::{Pread, Pwrite, LE};
 
-#[derive(NMread)]
+#[allow(dead_code)]
+const VERS_TAG: u32 = 432412312;
+
+#[derive(NMread, Debug)]
 pub struct VersionChunk {
+    #[nebula(tag = VERS_TAG)]
     pub chunk_size: u32,
     pub version: u32,
 }
 
 #[test]
 fn version_chunk_read_test() {
-    let chunk = VersionChunk { chunk_size: 0, version: 0 };
+    let bytes = [0xefu8, 0xbe, 0xad, 0xde, 142, 3, 0, 0];
+    let chunk: VersionChunk = bytes.pread_with(0, LE).unwrap();
+    println!("{:?}", &chunk);
 }
 
