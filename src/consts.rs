0a013fbc79808f6e647ea97dab7ac5445272e21d
Nebula Venus <nebulavenus@tuta.io>
2020-06-09 20:54:24 +0200

    Add new tags

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,6 @@
 pub const MDLX_TAG: u32 = 1481393229;
 
+// Main chunks
 pub const VERS_TAG: u32 = 1397900630;
 pub const MODL_TAG: u32 = 1279545165;
 pub const SEQS_TAG: u32 = 1397835091;
@@ -21,16 +22,79 @@ pub const EVTS_TAG: u32 = 1398036037;
 pub const CAMS_TAG: u32 = 1397571907;
 pub const CLID_TAG: u32 = 1145654339;
 
-pub const KTAT_TAG: u32 = 1413567563;
-pub const KTAR_TAG: u32 = 1380013131;
-pub const KTAS_TAG: u32 = 1396790347;
+// Node
 pub const KGTR_TAG: u32 = 1381254987;
-
 pub const KGRT_TAG: u32 = 1414678347;
 pub const KGSC_TAG: u32 = 1129531211;
 
+// Layer
+pub const KMTF_TAG: u32 = 1179929931;
+pub const KMTA_TAG: u32 = 1096043851;
+pub const KMTE_TAG: u32 = 1163152715;
+pub const KFC3_TAG: u32 = 860046923;
+pub const KFCA_TAG: u32 = 1094927947;
+pub const KFTC_TAG: u32 = 1129596491;
+
+// Texture animation
+pub const KTAT_TAG: u32 = 1413567563;
+pub const KTAR_TAG: u32 = 1380013131;
+pub const KTAS_TAG: u32 = 1396790347;
+
+// Geoset Animation
 pub const KGAO_TAG: u32 = 1329678155;
 pub const KGAC_TAG: u32 = 1128351563;
+
+// Light
+pub const KLAS_TAG: u32 = 1396788299;
+pub const KLAE_TAG: u32 = 1161907275;
+pub const KLAC_TAG: u32 = 1128352843;
+pub const KLAI_TAG: u32 = 1229016139;
+pub const KLBI_TAG: u32 = 1229081675;
+pub const KLBC_TAG: u32 = 1128418379;
+pub const KLAV_TAG: u32 = 1447119947;
+
+// Attachment
+pub const KATV_TAG: u32 = 1448362315;
+
+// Particle emitter
+pub const KPEE_TAG: u32 = 1162170443;
+pub const KPEG_TAG: u32 = 1195724875;
+pub const KPLN_TAG: u32 = 1313624139;
+pub const KPLT_TAG: u32 = 1414287435;
+pub const KPEL_TAG: u32 = 1279610955;
+pub const KPES_TAG: u32 = 1397051467;
+pub const KPEV_TAG: u32 = 1447383115;
+
+// Particle emitter 2
+pub const KP2E_TAG: u32 = 1160925259;
+pub const KP2G_TAG: u32 = 1194479691;
+pub const KP2L_TAG: u32 = 1278365771;
+pub const KP2S_TAG: u32 = 1395806283;
+pub const KP2V_TAG: u32 = 1446137931;
+pub const KP2R_TAG: u32 = 1379029067;
+pub const KP2N_TAG: u32 = 1311920203;
+pub const KP2W_TAG: u32 = 1462915147;
+
+// Ribbon emitter
+pub const KRVS_TAG: u32 = 1398166091;
+pub const KRHA_TAG: u32 = 1095258699;
+pub const KRHB_TAG: u32 = 1112035915;
+pub const KRAL_TAG: u32 = 1279349323;
+pub const KRCO_TAG: u32 = 1329812043;
+pub const KRTX_TAG: u32 = 1481921099;
+
+// Camera
+pub const KCTR_TAG: u32 = 1381253963;
+pub const KCRL_TAG: u32 = 1280459595;
+pub const KTTR_TAG: u32 = 1381258315;
+
+// Corn emitter
+pub const KPPA_TAG: u32 = 1095782475;
+pub const KPPC_TAG: u32 = 1129336907;
+pub const KPPE_TAG: u32 = 1162891339;
+pub const KPPL_TAG: u32 = 1280331851;
+pub const KPPS_TAG: u32 = 1397772363;
+pub const KPPV_TAG: u32 = 1448104011;
 
 // Geoset specific
 pub const VRTX_TAG: u32 = 1481921110;
