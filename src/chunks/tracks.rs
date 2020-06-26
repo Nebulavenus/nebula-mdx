ee2b1560fcd0629addb94918ce2a55203a581438
Nebula Venus <nebulavenus@tuta.io>
2020-06-27 00:05:32 +0200

    Introduce generic structs for tracks

--- src/chunks/tracks.rs
+++ src/chunks/tracks.rs
@@ -1,6 +1,78 @@
+#![warn(unused_assignments)]
+
 use crate::chunks::BytesTotalSize;
 use scroll::{ctx, Endian, Pread, Pwrite};
 use std::mem::size_of_val;
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Track<T> {
+    pub time: u32,
+    pub value: T,
+    pub in_tan: Option<T>,
+    pub out_tan: Option<T>,
+}
+
+impl<'a, T: 'a> ctx::TryFromCtx<'a, Endian> for Track<T>
+    where T: Default + ctx::TryFromCtx<'a, Endian, Error = scroll::Error>,
+{
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &'a [u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let time = src.gread_with::<u32>(offset, ctx)?;
+        let mut value = <T>::default();
+        value = src.gread_with(offset, ctx)?;
+
+        Ok((
+            Track {
+                time,
+                value,
+                in_tan: None,
+                out_tan: None,
+            },
+            *offset,
+        ))
+    }
+}
+
+impl<T> ctx::TryIntoCtx<Endian> for Track<T>
+    where T: Copy + Default + ctx::TryIntoCtx<Endian, Error = scroll::Error>,
+{
+    type Error = scroll::Error;
+
+    fn try_into_ctx(self, src: &mut [u8], ctx: Endian) -> Result<usize, Self::Error> {
+        let offset = &mut 0;
+
+        src.gwrite_with::<u32>(self.time, offset, ctx)?;
+        src.gwrite_with::<T>(self.value, offset, ctx)?;
+
+        if self.in_tan.is_some() {
+            src.gwrite_with::<T>(self.in_tan.unwrap(), offset, ctx)?;
+        }
+
+        if self.out_tan.is_some() {
+            src.gwrite_with::<T>(self.out_tan.unwrap(), offset, ctx)?;
+        }
+
+        Ok(*offset)
+    }
+}
+
+impl<T> BytesTotalSize for Track<T> {
+    fn total_bytes_size(&self) -> usize {
+        let mut result = 0usize;
+        result += size_of_val(&self.time);
+        result += size_of_val(&self.value);
+        if self.in_tan.is_some() {
+            result += size_of_val(self.in_tan.as_ref().unwrap());
+        }
+        if self.out_tan.is_some() {
+            result += size_of_val(self.out_tan.as_ref().unwrap());
+        }
+        result
+    }
+}
 
 macro_rules! create_named_track {
     ($name:ident, $typ:ty, $size:expr) => {
